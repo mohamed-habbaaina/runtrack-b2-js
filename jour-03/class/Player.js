f63defd15ebd2b70c0bd371e27ca2168c12deddf
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2023-09-08 15:30:26 +0200

    class Player, fixed bug

--- jour-03/class/Player.js
+++ jour-03/class/Player.js
@@ -1,7 +1,6 @@
-class Player {
+export default class Player {
     constructor(symbol, isCurrentPlayer) {
         this.symbol = symbol; // 'X' ou 'O'
         this.isCurrentPlayer = isCurrentPlayer; // true ou false
     }
 }
-module.exports = Player;
