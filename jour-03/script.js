92d3f9ee9516fbedcb267c9f93539e8cc9eba8ff
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2023-09-11 10:28:10 +0200

    Add import class and object jeux

--- jour-03/script.js
+++ jour-03/script.js
@@ -1,8 +1,12 @@
+import Board from './class/Board.js';
 import Player from './class/Player.js';
+// import "./style.css";
 
+const board = document.getElementById('board');
 
 const player1 = new Player('X', true);
 const player2 = new Player('O', false);
+const jeux = new Board()
 
 console.log(player1.symbol); // 'X'
 console.log(player2.isCurrentPlayer); // false
